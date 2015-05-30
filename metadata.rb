75124cf144903e542699a4dc02c4a6c96fc787d2
Dan Stark <dstark@wingspan.com>
2015-05-29 21:24:44 -0400

    need to do more research for php implementation

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'dstark75@gmail.com'
 license 'MIT'
 description 'Installs/Configures interview-buster'
 long_description 'A LAMP virtual machine with tags that are intentionally broken'
+
 version '0.1.2'
+
 depends 'mysql', '~> 5.5'
 depends 'httpd', '~> 2.2'
+depends 'php', '~> 1.3.14'
